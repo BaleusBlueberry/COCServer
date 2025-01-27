dfd541a92f4cdfd110cd1a52bbc4cce4f69e503d
BaleusBlueberry <ssaasshh29@gmail.com>
2025-01-27 23:45:00 +0200

    Rush to Submit update

--- DLA/Models/UserData/FavoriteBuildingsAndTownHallModel.cs
+++ DLA/Models/UserData/FavoriteBuildingsAndTownHallModel.cs
@@ -1,14 +1,8 @@
-﻿using AspNetCore.Identity.Mongo.Model;
-using DLA.Models.UserData;
-
-namespace DLA.Models
+﻿namespace DLA.Models.UserData
 {
-    public class AppUser : MongoUser
+    public class FavoriteBuildingsAndTownHallModel
     {
         public List<string> FavoriteTownHalls { get; set; } = new List<string>();
         public FavoriteBuildingsModel FavoriteBuildings { get; set; } = new FavoriteBuildingsModel();
-    }
-    public class AppRole : MongoRole
-    {
     }
 }
---
+++ DLA/Models/UserData/FavoriteBuildingsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DLA.Models.UserData
+{
+    public class FavoriteBuildingsModel
+    {
+        public List<string> ArmyBuildings { get; set; } = new List<string>();
+        public List<string> DefensiveBuildings { get; set; } = new List<string>();
+        public List<string> ResourceBuildings { get; set; } = new List<string>();
+        public List<string> TrapBuildings { get; set; } = new List<string>();
+    }
+}
